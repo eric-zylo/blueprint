9266b4919f8a4d136b91abcb159ec6415c8327f1
Eric <eric@Erics-MBP.attlocal.net>
2025-03-25 03:03:47 -0400

    Add Confirmable to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
